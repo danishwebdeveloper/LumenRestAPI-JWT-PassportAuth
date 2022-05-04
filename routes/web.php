d0989be263848a90722dc5e0266a92222568c4a3
MuhammadDanish <danishbutt.uos@gmail.com>
2022-05-04 10:41:56 +0200

    REST API with Passport and JWT Auth

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ $router->group(['prefix' => 'lumenapi'], function () use ($router) {
     $router->post('/register', 'AuthController@register');
     $router->post('/login', 'AuthController@login');
 
-    // $router->group(['middleware' => 'auth'], function () use ($router) {
-    $router->post('/logout', 'AuthController@logout');
-    $router->get('/posts', 'PostController@index');
-    $router->post('/posts', 'PostController@store');
-    $router->put('/posts/{id}', 'PostController@update');
-    $router->delete('/posts/{id}', 'PostController@destroy');
-    // });
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        $router->post('/logout', 'AuthController@logout');
+        $router->get('/posts', 'PostController@index');
+        $router->post('/posts', 'PostController@store');
+        $router->put('/posts/{id}', 'PostController@update');
+        $router->delete('/posts/{id}', 'PostController@destroy');
+    });
 });
